1212ad828ce992eff907b8a003c0418cd7fdb68e
ckxckx <ckx1025ckx@gmail.com>
2025-01-24 22:33:36 +0800

    问题定位+初次发布

--- src/extension.ts
+++ src/extension.ts
@@ -2,32 +2,12 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-// export function activate(context: vscode.ExtensionContext) {
-
-// 	// Use the console to output diagnostic information (console.log) and errors (console.error)
-// 	// This line of code will only be executed once when your extension is activated
-// 	console.log('Congratulations, your extension "tag-pinescript-functions" is now active!');
-
-// 	// The command has been defined in the package.json file
-// 	// Now provide the implementation of the command with registerCommand
-// 	// The commandId parameter must match the command field in package.json
-// 	const disposable = vscode.commands.registerCommand('tag-pinescript-functions.helloWorld', () => {
-// 		// The code you place here will be executed every time your command is executed
-// 		// Display a message box to the user
-// 		vscode.window.showInformationMessage('xxxxx from tag_pinescript_functions!');
-// 	});
-
-// 	context.subscriptions.push(disposable);
-// }
-
-
-
 /**
  * Activate the extension.
  * @param context The extension context.
  */
+
+
 export function activate(context: vscode.ExtensionContext): void {
     // Register a document symbol provider for Pine Script files.
     const pineScriptSymbolProvider = vscode.languages.registerDocumentSymbolProvider(
